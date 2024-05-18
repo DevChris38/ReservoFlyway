0e9b79284265b697ead47df40600d70750fb6f38
christopher <christopher.arthaud@gmail.com>
2024-05-18 15:33:01 +0200

    First commit
    First commit

---
+++ src/main/java/com/reservo/reservoflyway/ReservoFlywayApplication.java
@@ -0,0 +1,13 @@
+package com.reservo.reservoflyway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservoFlywayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReservoFlywayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/reservo/reservoflyway/ReservoFlywayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reservo.reservoflyway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservoFlywayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
